d93ab3e99abc5408c6db5f069f31473ac0900e45
Jonathan Tarrant <jmtarrant00@gmail.com>
2023-09-18 17:55:02 -0400

    Add Erosion and Dilation

--- Assignment_3/morphology.m
+++ Assignment_3/morphology.m
@@ -2,3 +2,40 @@
 % Number: 000-831-487
 % Assignment 3
 
+% Load image into program
+image = imread("morphology.png");
+
+
+%%% EROSION %%%
+
+% Define the Structuring Element
+structuring_element = strel('square', 5);
+
+
+% Perform erosion on the image
+eroded_image = imerode(image,structuring_element);
+
+
+%%% DILATION %%%
+
+% No need to redefine a structuring element 
+
+% Perform Dilation on the image
+dilated_image = imdilate(image, structuring_element);
+
+% Display the images
+subplot(2, 2, 1);
+imshow(image);
+title("Original Image")
+
+subplot(2, 2, 2);
+imshow(eroded_image);
+title("Eroded Image");
+
+subplot(2, 2, 3);
+imshow(image);
+title("Original Image");
+
+subplot(2, 2, 4);
+imshow(dilated_image);
+title("Dilated Image");
