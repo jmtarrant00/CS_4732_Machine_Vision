b605991b36b24635192a884c0fe66c115a89d05a
Starwolf16 <jmtarrant00@gmail.com>
2023-09-19 21:15:55 -0400

    Update cells.m

--- Assignment_3/cells.m
+++ Assignment_3/cells.m
@@ -5,23 +5,29 @@
 % Read the image into the program
 cells_image = imread("cell.jpg");
 
+
 % Greyscale the image
 grey_cells = rgb2gray(cells_image);
 
+
 % Apply Thresholding
 threshold_image = imbinarize(grey_cells);
+
 
 % Apply Dilation to the image to make sure all connected cell parts
 structural = strel('disk', 10);
 closed_image = imclose(threshold_image, structural);
 
+
 % Find the connected componenets (count the cells)
 connected_components = bwconncomp(closed_image);
 disp(connected_components)
 
+
 % Find the biggest cell
 cell_sizes = cellfun(@numel, connected_components.PixelIdxList);
 [~, biggest_cell_index] = max(cell_sizes);
+
 
 % Get the boundary Image of the largest cell
 boundary_image = false(size(closed_image));
@@ -48,6 +54,7 @@ title("Morphological Closing")
 subplot(3, 2, 5);
 imshow(boundary_image);
 title("Largest Cell")
+
 
 % Print the number of cells 
 disp(['Number of Cells: ', num2str(connected_components.NumObjects)])
