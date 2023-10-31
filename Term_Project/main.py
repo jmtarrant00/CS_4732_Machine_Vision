ec13bf7f0abe7c252c53a71b786dc63a720162b6
Starwolf16 <jmtarrant00@gmail.com>
2023-10-30 22:06:38 -0400

    finish Training loop

--- Term_Project/main.py
+++ Term_Project/main.py
@@ -1,14 +1,21 @@
 import argparse
+import numpy as np
 import time
 import torch
+import torch.amp
 import torchvision
 import torchvision.transforms as transforms
-import vae_network
+import tqdm as tqdm
+import vae_network as vae_network
 
 from sys import exit
 from time import time
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from torchvision.datasets import ImageFolder
+
+# Print out the device the model is running on 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f'PyTorch is running on {device}')
 
 ### ARGUMENT PARSING ###
  
@@ -17,7 +24,7 @@ parser = argparse.ArgumentParser()
 
 # Set the arguments for the parser
 parser.add_argument('-e', '--epochs', type=int, help='Specify the number of epochs for the training loop to run.')
-parser.add_argument('-lr', '--learning_rate', type=float, default=0.001,
+parser.add_argument('-lr', '--learning_rate', type=float, default=0.0001,
                     help='Specify the learning rate for the training phase. Default value is 0.001.')
 parser.add_argument('-c', '--checkpoint', type=str, help='Give the path for a checkpoint to load a model from.')
 
@@ -39,20 +46,74 @@ dataset = ImageFolder('dataset/', transform=transform)
 image, label = dataset[0]
 depth, width, height = image.shape
 
+# Split the data into training and testing sets
+num_images = len(dataset)
+train_size = int(num_images * 0.6)
+val_size = int(num_images * 0.1)
+test_size = num_images - train_size - val_size
+
+train_set, validation_set, test_set = random_split(dataset, [train_size, val_size, test_size])
+
+train_loader = DataLoader(train_set, batch_size = 250, shuffle=True)
+validation_loader = DataLoader(validation_set, batch_size=35, shuffle=True)
+test_loader = DataLoader(test_set, batch_size=100, shuffle=False)
+
 ### MODEL SET-UP ###
 
 # Initialize the model
 if not args.checkpoint:
     model = vae_network.VAE_NETWORK(depth=depth, width=width, height=height, latent_size=16)
+    # Initialize the optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
 else:
     kwargs, model = torch.load('model.pth')
     model = vae_network.VAE_NETWORK(**kwargs)
     model.load_state_dict(model)
 
-print(model)
+# Move the model to the gpu if available
+model.to(device)
+
+# Save the model architecture to a file
+with open('model_architecture.txt', 'w') as architecture_file:
+    print(model, file=architecture_file)
+architecture_file.close()
+
 
 ### TRAINING PHASE ###
 
+# Set the autocast type based on the device
+if device == 'cuda':
+    autocast_type = torch.float16
+else:
+    autocast_type = torch.bfloat16
+
+# Set up a list for losses
+loss_list = []
+
+start_time = time()
+for epoch in range(args.epochs):
+    for images, labels in train_loader:
+        with torch.autocast(device_type=device, dtype=autocast_type):
+            # Move the tensors to device
+            images = images.to(device)
+            labels = labels.to(device)
+        
+            # Run the images through the network
+            outputs, mean, log_var = model(images)
+            loss = model.loss_fn(images, outputs, mean, log_var)
+        
+        loss.backward()
+        optimizer.step()
+
+        loss_list.append(loss.item())
+
+end_time = time()
+# Print out some training stats
+print('\n----- TRAINING STATS -----')
+print(f'Training time: {(end_time - start_time):.2f}')
+print(f'Max Loss: {np.max(loss_list)}')
+print(f'Min Loss: {np.min(loss_list)}')
+print(f'Average Loss: {np.average(loss_list):.3f}\n')
 
 
 ### TESTING PHASE
