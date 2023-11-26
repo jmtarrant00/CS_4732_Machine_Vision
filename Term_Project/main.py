3ef3564904c35a46cf93c12fa8bd189facd7343c
Starwolf16 <jmtarrant00@gmail.com>
2023-11-26 18:45:43 -0500

    Add training progress bar and gradscaler

--- Term_Project/main.py
+++ Term_Project/main.py
@@ -1,17 +1,19 @@
 import argparse
+import matplotlib.pyplot as plt
 import numpy as np
 import time
 import torch
 import torch.amp
 import torchvision
 import torchvision.transforms as transforms
-import tqdm as tqdm
 import vae_network as vae_network
 
 from sys import exit
 from time import time
 from torch.utils.data import DataLoader, random_split
 from torchvision.datasets import ImageFolder
+from torch.cuda.amp import GradScaler
+from tqdm import tqdm
 
 # Print out the device the model is running on 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -27,6 +29,7 @@ parser.add_argument('-e', '--epochs', type=int, help='Specify the number of epoc
 parser.add_argument('-lr', '--learning_rate', type=float, default=0.0001,
                     help='Specify the learning rate for the training phase. Default value is 0.001.')
 parser.add_argument('-c', '--checkpoint', type=str, help='Give the path for a checkpoint to load a model from.')
+parser.add_argument('-g', dest='graph', action='store_true', help='Saves a graph of the loss over epochs')
 
 # Parse the arguments and get the namespace for them
 args = parser.parse_args()
@@ -65,6 +68,7 @@ if not args.checkpoint:
     model = vae_network.VAE_NETWORK(depth=depth, width=width, height=height, latent_size=16)
     # Initialize the optimizer
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+    scaler = GradScaler()
 else:
     kwargs, model = torch.load('model.pth')
     model = vae_network.VAE_NETWORK(**kwargs)
@@ -90,22 +94,30 @@ else:
 # Set up a list for losses
 loss_list = []
 
-start_time = time()
-for epoch in range(args.epochs):
-    for images, labels in train_loader:
-        with torch.autocast(device_type=device, dtype=autocast_type):
-            # Move the tensors to device
-            images = images.to(device)
-            labels = labels.to(device)
-        
-            # Run the images through the network
-            outputs, mean, log_var = model(images)
-            loss = model.loss_fn(images, outputs, mean, log_var)
-        
-        loss.backward()
-        optimizer.step()
+pbar_update = args.epochs / 10
 
+start_time = time()
+with tqdm(total=100) as pbar:
+    for epoch in range(args.epochs):
+        for images, labels in train_loader:
+            with torch.autocast(device_type=device, dtype=autocast_type):
+                # Move the tensors to device
+                images = images.to(device)
+                labels = labels.to(device)
+            
+                # Run the images through the network
+                outputs, mean, log_var = model(images)
+                loss = model.loss_fn(images, outputs, mean, log_var)
+
+            optimizer.zero_grad()
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+
+            pbar.update(pbar_update)
         loss_list.append(loss.item())
+        
+
 
 end_time = time()
 # Print out some training stats
@@ -115,5 +127,14 @@ print(f'Max Loss: {np.max(loss_list)}')
 print(f'Min Loss: {np.min(loss_list)}')
 print(f'Average Loss: {np.average(loss_list):.3f}\n')
 
+
+# Graph the loss if asked for 
+if args.graph:
+    plt.figure()
+    plt.plot([x for x in range(args.epochs)], loss_list)
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title('Loss over Epochs')
+    plt.savefig('loss_plot.svg')
 
 ### TESTING PHASE
